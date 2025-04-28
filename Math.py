8d6e7d3c96d20d8dfab7a248916f1b62d976202d
Akshaj6 <akshaj.samaria@gmail.com>
2025-04-28 17:56:46 +0530

    48

---
+++ Math.py
@@ -0,0 +1,11 @@
+import math
+x = 10
+y = -22
+print("The sign of x after copying the sign of y is: ", math.copysign(x, y))
+num1 = 34
+num2 = 45
+print(math.gcd(num1, num2))
+print(math.lcm(num1, num2))
+print(math.floor(23.799901))
+print(math.ceil(23.799901))
+print(math.fabs(-96))
